c5a5b86ea9c2a43524b49e008df4e52b416a1e30
Bruce <78635323+BruceResearch@users.noreply.github.com>
2022-05-05 21:54:16 -0400

    Update 5.Mnist_adv_set_make_PSO.py

--- Adv_Image_Generation/5.Mnist_adv_set_make_PSO.py
+++ Adv_Image_Generation/5.Mnist_adv_set_make_PSO.py
@@ -8,7 +8,7 @@ import Mnist_PSO_critical_point_adv_make
 sheet_id = 2
 
 your_path = '/your_path_to_main_dir'
-array_pic_num_mnist_all = np.load(str(your_path) + '/FI_Image_Choose/adv_info/sample_info/Mnist_sheet(%d)_situation(1)_FI_below(0.20)_pro_y_target(0.00)_array.npy'%(sheet_id))
+array_pic_num_mnist_all = np.load(str(your_path) + '/FI_Image_Choose/adv_info/sample_info/Mnist_sheet(%d)_situation(1)_FI_below(0.01)_pro_y_target(0.00)_array.npy'%(sheet_id))
 
 cut_start = 0
 cut_end =  len(array_pic_num_mnist_all)        # 136 / 26
@@ -35,6 +35,6 @@ for i_pic in range(cut_start,cut_end,1):  #  [0:correct_class, 1:target_class, 2
                                                                       correct_class=correct_class_i,
                                                                       target_class=target_class_i,
                                                                       sheet_id=sheet_id)  # (1,3072)
-    np.save(str(your_path) + '/FI_Image_Choose/adv_info/adv_Mnist/train/x/adv(%d)_mnist(%d)_sheet(%d)_situation(1)_FI_below(0.20)_pro_y_target(0.20)_POS_critical_point_adv_array.npy' % (i_pic,pic_num_i, sheet_id),x_adv_of_i)
-    np.save(str(your_path) + '/FI_Image_Choose/adv_info/adv_Mnist/train/y/adv(%d)_mnist(%d)_sheet(%d)_situation(1)_FI_below(0.20)_pro_y_target(0.20)_POS_critical_point_adv_array.npy' % (i_pic,pic_num_i, sheet_id),y_adv_of_i)
+    np.save(str(your_path) + '/FI_Image_Choose/adv_info/adv_Mnist/train/x/adv(%d)_mnist(%d)_sheet(%d)_situation(1)_FI_below(0.01)_pro_y_target(0.20)_POS_critical_point_adv_array.npy' % (i_pic,pic_num_i, sheet_id),x_adv_of_i)
+    np.save(str(your_path) + '/FI_Image_Choose/adv_info/adv_Mnist/train/y/adv(%d)_mnist(%d)_sheet(%d)_situation(1)_FI_below(0.01)_pro_y_target(0.20)_POS_critical_point_adv_array.npy' % (i_pic,pic_num_i, sheet_id),y_adv_of_i)
 
